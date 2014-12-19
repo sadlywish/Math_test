17caef8b7d9f31804267a2d1925453da46e03be1
sadlywish <fred0@sina.com>
2014-12-20 00:31:33 +0800

    修复文件名重复出现的BUG

--- app.js
+++ app.js
@@ -56,15 +56,22 @@ app.use(function(err, req, res, next) {
     });
 });*/
 
+
+module.exports = app;
+
 var fileList =new Array();
 myModule.ls('./src','js',function(err, data){
     if(err)
         throw err;
     fileList = data;
 });
-module.exports = app;
-var string = 'START:\n'
 module.exports.get('/',function(req,res){
+    myModule.ls('./src','js',function(err, data){
+        if(err)
+            throw err;
+        fileList = data;
+    });
+    var string = 'START:\n'
     for(i=0 ;i<fileList.length ;i++){
        string = string+ fileList[i]+'\n';
     }
