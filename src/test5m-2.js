3a8d1eb1c31c1094f25ba05e911644d781e4ffad
sadlywish <fred0@sina.com>
2014-12-20 01:38:21 +0800

    完成测试方法范例

--- src/test5m-2.js
+++ src/test5m-2.js
@@ -2,7 +2,7 @@ var ls = function (dir,suffix,callback){
 	var fs = require('fs');
 	var path = require('path');
 	var filelist = new Array();
-    console.log('start');
+    console.log(dir);
 	buf = fs.readdir(dir,function(err,date){
 		if(err){
 			return callback(err);
@@ -10,11 +10,9 @@ var ls = function (dir,suffix,callback){
 		for(i=0;i<date.length;i++){
 				if(path.extname(date[i])==('.'+suffix))
 				filelist.push(date[i]);
-				console.log(date[i]);
 		}
 		callback(null,filelist);
 
 	});
-    console.log(buf);
 };
 exports.ls = ls;
